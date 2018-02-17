6c6e013203b7579f965e7c7686eeaf5d0081d547
Caleb Rose <clrose86@gmail.com>
2018-02-16 18:30:27 -0600

    server side

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+var app = express();
+var port = process.env || 3000;
+
+app.use(express.static(__dirname + "/app/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
+});
